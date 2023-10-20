a17f4b4d70305e2c4e2d996a45cafe0a2240574a
amShuri <106229595+amShuri@users.noreply.github.com>
2023-10-20 16:47:51 -0300

    Create variables for hard-coded values to reduce repetition
    Additionally, this update improves the <table> population test. Instead of testing for the quantity of <tr> elements, we test for the content of <td> elements directly, providing a more comprehensive and reliable test.

--- cypress/e2e/currency-tests.cy.js
+++ cypress/e2e/currency-tests.cy.js
@@ -14,18 +14,20 @@ describe('Website loads correctly', () => {
 });
 
 describe('Data fetches correctly', () => {
-  it('contains the currency codes', () => {
+  it('contains the correct amount of available currencies', () => {
+    const availableCurrencies = 31;
+
     cy.get('#currency-base')
       .find('option')
-      .should('satisfy', (options) => options.length > 20);
+      .should('satisfy', (options) => options.length >= availableCurrencies);
 
     cy.get('#currency-target')
       .find('option')
-      .should('satisfy', (options) => options.length > 20);
+      .should('satisfy', (options) => options.length >= availableCurrencies);
 
     cy.get('#exchange-rates')
       .find('option')
-      .should('satisfy', (options) => options.length > 20);
+      .should('satisfy', (options) => options.length >= availableCurrencies);
 
     cy.get('#currency-base')
       .select(5)
@@ -57,34 +59,34 @@ describe('Performs currency conversions', () => {
     });
   });
 
-  it('throws an error when providing invalid inputs', () => {
+  it('throws an error when providing an invalid amount or an empty currency', () => {
+    const emptyCurrencyError = 'Select a currency to proceed.';
     cy.get('#conversion-amount').type('-1');
     cy.get('#convert-button').click();
     cy.get('#conversion-amount + span').should(
       'contain',
       'The amount must be a positive number.'
     );
-    cy.get('#currency-base + span').should('contain', 'Select a currency to proceed.');
-    cy.get('#currency-target + span').should('contain', 'Select a currency to proceed.');
+    cy.get('#currency-base + span').should('contain', emptyCurrencyError);
+    cy.get('#currency-target + span').should('contain', emptyCurrencyError);
   });
 
   it('throws an error when providing identical currencies', () => {
+    const identicalCurrencyError = 'Currencies cannot be identical.';
     cy.get('#currency-base').select(2);
     cy.get('#currency-target').select(2);
     cy.get('#convert-button').click();
-    cy.get('#currency-base + span').should('contain', 'Currencies cannot be identical.');
-    cy.get('#currency-target + span').should(
-      'contain',
-      'Currencies cannot be identical.'
-    );
+    cy.get('#currency-base + span').should('contain', identicalCurrencyError);
+    cy.get('#currency-target + span').should('contain', identicalCurrencyError);
   });
 });
 
 describe('Table populates correctly', () => {
   it('shows exchange rates successfully', () => {
+    const validExchangeRate = /^[0-9]/g;
     cy.get('#exchange-rates').select(3);
     cy.get('tbody')
-      .find('tr')
-      .should('satisfy', (elements) => elements.length > 10);
+      .find('td')
+      .should('satisfy', (tableData) => tableData.text().match(validExchangeRate));
   });
 });
